22686ab41a265f93001488113e8ab2c099ac4c63
Bruce Dunwiddie <shriop@hotmail.com>
2020-05-25 00:41:52 -0500

    Added example of calling method within method.

--- LinqExpressionsTests/LinqExpressionsTests/MethodTests.cs
+++ LinqExpressionsTests/LinqExpressionsTests/MethodTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Reflection;
 
@@ -43,7 +44,7 @@ namespace LinqExpressionsTests
 				// this is going to be the function body/logic
 				body,
 				
-				// these is the parameter list being passed in to the lambda body
+				// this is the parameter list being passed in to the lambda body
 
 				// have to use same ParameterExpression instances referenced in
 				// expression body definition above to get them to map through Func call
@@ -105,7 +106,7 @@ namespace LinqExpressionsTests
 				// the value returned from a function is the last expression in its body
 				body,
 
-				// these is the parameter list being passed in to the lambda body
+				// this is the parameter list being passed in to the lambda body
 
 				// have to use same ParameterExpression instances referenced in
 				// expression body definition above to get them to map through Func call
@@ -120,6 +121,148 @@ namespace LinqExpressionsTests
 			Console.WriteLine(formattedDate);
 
 			Assert.AreEqual("01:23:45", formattedDate);
+		}
+
+		[Test]
+		public void Method_InstanceMethodWithNoReturn()
+		{
+			List<string> words = new List<string>() { "one", "two" };
+
+			// locate the method to be called
+			MethodInfo add = words
+				.GetType()
+				.GetMethod(
+					"Add", 
+					new Type[] { typeof(string) });
+
+			// ParameterExpression is used to map a value from the caller
+			// to the lambda body
+
+			// this is the same for object instances as well as method
+			// parameters
+			ParameterExpression list = Expression.Parameter(
+				words.GetType());
+
+			ParameterExpression word = Expression.Parameter(
+				typeof(string));
+
+			// define the body of the lambda
+
+			// we're going to just have a single method call within the body
+			MethodCallExpression body = Expression.Call(
+				list,
+				add,
+				new Expression[] { word });
+
+			// Action<in T1, in T2>
+
+			Expression<Action<List<string>, string>> lambda = Expression.Lambda<Action<List<string>, string>>(
+				
+				// this is going to be the function body/logic
+
+				// the value returned from a function is the last expression in its body
+				body,
+
+				// this is the parameter list being passed in to the lambda body
+
+				// have to use same ParameterExpression instances referenced in
+				// expression body definition above to get them to map through Func call
+
+				// remember to add instance parameter
+				new ParameterExpression[] { list, word });
+
+			// create the Action from the lambda
+			Action<List<string>, string> action = lambda.Compile();
+
+			// use Action to execute method
+			action(words, "three");
+
+			string wordsList = string.Join(",", words);
+
+			Console.WriteLine(wordsList);
+
+			Assert.AreEqual(3, words.Count);
+			Assert.AreEqual("three", words[2]);
+		}
+
+		[Test]
+		public void Method_InnerMethod()
+		{
+			List<string> dates = new List<string>();
+
+			// locate the methods to be called
+			MethodInfo add = dates
+				.GetType()
+				.GetMethod(
+					"Add",
+					new Type[] { typeof(string) });
+
+			MethodInfo toString = typeof(DateTime)
+				.GetMethod(
+					"ToString",
+					new Type[] { typeof(string) });
+
+			// ParameterExpression is used to map a value from the caller
+			// to the lambda body
+
+			// this is the same for object instances as well as method
+			// parameters
+			ParameterExpression list = Expression.Parameter(
+				dates.GetType());
+
+			ParameterExpression date = Expression.Parameter(
+				typeof(DateTime));
+
+			// use Expression.Constant to convert a 'normal' value into an expression
+			ConstantExpression format = Expression.Constant(
+				"MMM d");
+
+			MethodCallExpression toStringCall = Expression.Call(
+				date,
+				toString,
+				new Expression[] { format });
+
+			// define the body of the lambda
+
+			// we're passing in the body of one method call in as a
+			// parameter to another method call
+			MethodCallExpression body = Expression.Call(
+				list,
+				add,
+				new Expression[] { toStringCall });
+
+			Expression<Action<List<string>, DateTime>> lambda = Expression.Lambda<Action<List<string>, DateTime>>(
+				
+				// this is going to be the function body/logic
+
+				// the value returned from a function is the last expression in its body
+				body,
+
+				// this is the parameter list being passed in to the lambda body
+
+				// have to use same ParameterExpression instances referenced in
+				// expression body definition above to get them to map through Func call
+				new ParameterExpression[] { list, date });
+
+			// create the Action from the lambda
+			Action<List<string>, DateTime> action = lambda.Compile();
+
+			// use Action to execute method
+			
+			// this will format the date using the specified format
+			// and add the formatted string to the list
+			action(dates, DateTime.Parse("1/1/2020"));
+			action(dates, DateTime.Parse("1/2/2020"));
+			action(dates, DateTime.Parse("2/1/2020"));
+
+			string datesAsString = string.Join(",", dates);
+
+			Console.WriteLine(datesAsString);
+
+			Assert.AreEqual(3, dates.Count);
+			Assert.AreEqual("Jan 1", dates[0]);
+			Assert.AreEqual("Jan 2", dates[1]);
+			Assert.AreEqual("Feb 1", dates[2]);
 		}
 	}
 }
